4347dadb64eb67bcc56f144aa5127f17abe0960c
buildwithcode56-dot <buildwithcode56@gmail.com>
2026-08-30 08:10:52 -0700

    Create InvalidPaymentException.java

---
+++ InvalidPaymentException.java
@@ -0,0 +1,7 @@
+package com.mycompany.ecommerceproductsystem;
+public class InvalidPaymentException extends Exception
+  {
+    public InvalidPaymentException(String message) {
+        super(message);
+    }
+}
